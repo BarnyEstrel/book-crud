7d8bef2008516b51b86366b361b1457a0ba577ea
Barny Estrel <bryanlester.fundar@gmail.com>
2022-05-27 21:15:45 +0800

    controllers for novel, and revision for routings and file name

---
+++ app/Http/Controllers/NovelController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NovelController extends Controller
+{
+    //show all novels
+
+    public function index(){
+        return view('novels/index');
+    }
+
+    public function create(){
+        
+    }
+
+    public function update(){
+        
+    }
+
+    public function delete(){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NovelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('novels');
-});
+Route::get('/', [NovelController::class, 'index']);
+Route::get('/create', [NovelController::class, '']);
+Route::get('/update', [NovelController::class, '']);
+Route::get('/delete', [NovelController::class, '']);
